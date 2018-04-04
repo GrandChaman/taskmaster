9ae67d3ee844909d3bff1b5664cb10fb24e363ca
Francis LE ROY <francis.leroy.ch@gmail.com>
2018-04-04 17:51:15 +0200

    Client and server talking over unix socket successfully

--- client.js
+++ client.js
@@ -1,17 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.js                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fle-roy <fle-roy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/04 17:42:52 by fle-roy           #+#    #+#             */
+/*   Updated: 2018/04/04 17:50:25 by fle-roy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 const readline = require('readline');
 const base_prompt = "$ TaskMaster_> " + "\x1b[0m";
+const sock_path = __dirname + "/taskmaster.sock";
+const net = require('net');
+
+function completer(line) {
+  const completions = 'list status start stop reload config help exit'.split(' ');
+  const hits = completions.filter((c) => c.startsWith(line));
+  return [hits.length ? hits : completions, line];
+}
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: true,
-    //completer: completer,
+    completer: completer,
     historySize: 100,
     removeHistoryDuplicates: true,
-
     prompt: "\x1b[1;32m" + base_prompt
 });
 
-rl.prompt();
+var socket = net.Socket();
+socket.connect(sock_path, () => {
+	console.log("[INFO] Connected to socket");
+	rl.prompt();
+});
+socket.on("error", (err) => {
+	console.error("[ERROR] Error on socket\n" + err);
+	process.exit(1);
+});
+socket.on("data", (data) => {
+	console.log(data.toString("UTF-8"));
+	rl.prompt();
+});
 rl.on("close", () => {
     console.log("exit");
     process.exit(0);
@@ -24,24 +56,5 @@ rl.on('SIGCONT', () => {
 });
 
 rl.on('line', (line) => {
-    var line_array = line.trim().split(/\s+/g);
-    switch (line_array[0]) {
-        case "":
-            break;
-        case "status":
-        case "start":
-        case "stop":
-        case "reload":
-        case "config":
-        case "help":
-            console.log('world!');
-            break;
-        case "exit":
-            rl.close();
-            break;
-        default:
-            console.log("Command not found. Try help next time ;)");
-            break;
-    }
-    rl.prompt();
+	socket.write(line);
 });
---
+++ server.js
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.js                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fle-roy <fle-roy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/04 16:27:22 by fle-roy           #+#    #+#             */
+/*   Updated: 2018/04/04 17:49:40 by fle-roy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+const net = require('net');
+const fs = require('fs');
+const sock_path = __dirname + "/taskmaster.sock";
+
+var server = net.createServer((socket) => {
+	socket.on("connect", () =>{console.log("[INFO] Socket connected to server")});
+	socket.on("close", () =>{console.log("[INFO] Socket disconnected from server")});
+	socket.on("error", (err) =>{console.error("[ERROR] Socket error\n" + err)});
+	socket.on("data", (data) =>
+	{
+		let line_array = data.toString().trim().split(/\s+/g);
+	    switch (line_array[0]) {
+	        case "":
+	            break;
+			case "list":
+	        case "status":
+	        case "start":
+	        case "stop":
+	        case "reload":
+	        case "config":
+	        case "help":
+	            socket.write("GOTCHA");
+	            break;
+	        case "exit":
+	            rl.close();
+	            break;
+	        default:
+	            socket.write("Command not found. Try help next time ;)");
+	            break;
+	    }
+	});
+});
+
+function clean_up(closing)
+{
+	try {
+		if (fs.existsSync(sock_path))
+			fs.unlinkSync(sock_path);
+	} catch (err)
+	{
+		console.error("[ERROR] Can't deleting existing socket\n" + err);
+		process.exit(1);
+	}
+	if (closing)
+	{
+		console.log("[INFO] Removed UNIX socket");
+		console.log("[INFO] Closing server");
+		server.close(() => {console.log("[INFO] Closed server");});
+		process.removeAllListeners();
+	}
+}
+
+clean_up(0);
+
+server.listen(sock_path);
+process.once("exit", () => {clean_up(1);});
+process.once("SIGTERM", () => {clean_up(1);});
+process.once("SIGINT", () => {clean_up(1);});
+process.once("SIGUSR1", () => {clean_up(1);});
+process.once("SIGUSR2", () => {clean_up(1);});
+process.once("unhandledException", () => {clean_up(1);});
