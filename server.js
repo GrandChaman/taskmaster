29a484e73db7adcbadea0a651fc0d5f9343f5dc6
Francis Le Roy <francis.leroy.ch@gmail.com>
2018-04-11 12:55:37 +0200

    Can spawn process, need to support multiple instance of the same process

--- server.js
+++ server.js
@@ -6,18 +6,104 @@
 /*   By: fle-roy <fle-roy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/04 16:27:22 by fle-roy           #+#    #+#             */
-/*   Updated: 2018/04/04 17:56:13 by fle-roy          ###   ########.fr       */
+/*   Updated: 2018/04/11 12:35:49 by bluff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 const net = require('net');
 const fs = require('fs');
 const sock_path = __dirname + "/taskmaster.sock";
+const { spawn } = require('child_process');
+const config_handle = require("./config.js");
+var child_vector = [];
+var config_obj;
 
+config_obj = config_handle.read_config();
+console.log(config_obj);
+function find_in_child_vector(value)
+{
+	return (child_vector.find(function(element) {
+		return (element.name === value);
+	}));
+}
+
+function tm_log(message)
+{
+	let time = process.uptime();
+
+	console.log('[' + time.toFixed(Math.floor((time / 10)) + 3) + '] [INFO] ' + message);
+}
+
+function tm_error(message)
+{
+	let time = process.uptime();
+
+	console.log('[' + time.toFixed((time / 10) + 3) + '] [INFO] ' + message);
+}
+
+function load_child()
+{
+	config_obj.prgm_list.forEach((el) => {
+		if (find_in_child_vector(el.name))
+			return ;
+		el.proc = undefined;
+		el.restart_nbr = 0;
+		el.start_time = 0;
+		child_vector.push(el);
+	});
+}
+
+function child_guard(child, code, signal)
+{
+	if (!signal)
+		tm_log(child.name + " exited [" + code + "]");
+	else
+		tm_log(child.name + " :  [" + signal + "]");
+}
+
+function launch_child(child)
+{
+	tm_log("Starting " + child.name);
+	let command_splitted = child.command.split(' ');
+	let fd_stdout = null;
+	let fd_stderr = null;
+	if (child.stdout_logfile)
+		fd_stdout = fs.openSync(child.stdout_logfile, 'w', 0o644);
+	if (child.stderr_logfile)
+		fd_stderr = fs.openSync(child.stderr_logfile, 'w', 0o644);
+	let old_mask = process.umask(child.umask);
+	child.proc = spawn(command_splitted.shift(), command_splitted, {
+		cwd: child.cwd,
+		env: child.env,
+		detached: false,
+		stdio: [null, fd_stdout, fd_stderr]
+	});
+	child.start_time = process.uptime();
+	child.proc.on('exit', (code, signal) => {
+		child_guard(child, code, signal);
+	});
+	process.umask(old_mask);
+	if (fd_stdout != null)
+		fs.closeSync(fd_stdout);
+	if (fd_stderr != null)
+		fs.closeSync(fd_stderr);
+	tm_log("Started " + child.name);
+}
+
+function launch_child_startup()
+{
+	child_vector.forEach((el) => {
+		if (!el.start_at_launch)
+			return ;
+		launch_child(el);
+	});
+}
+load_child();
+launch_child_startup();
 var server = net.createServer((socket) => {
-	socket.on("connect", () =>{console.log("[INFO] Socket connected to server")});
-	socket.on("close", () =>{console.log("[INFO] Socket disconnected from server")});
-	socket.on("error", (err) =>{console.error("[ERROR] Socket error\n" + err)});
+	socket.on("connect", () =>{tm_log("Socket connected to server")});
+	socket.on("close", () =>{tm_log("Socket disconnected from server")});
+	socket.on("error", (err) =>{tm_error("Socket error\n" + err)});
 	socket.on("data", (data) =>
 	{
 		let line_array = data.toString().trim().split(/\s+/g);
@@ -47,14 +133,14 @@ function clean_up(closing)
 			fs.unlinkSync(sock_path);
 	} catch (err)
 	{
-		console.error("[ERROR] Can't deleting existing socket\n" + err);
+		tm_error("Can't deleting existing socket\n" + err);
 		process.exit(1);
 	}
 	if (closing)
 	{
-		console.log("[INFO] Removed UNIX socket");
-		console.log("[INFO] Closing server");
-		server.close(() => {console.log("[INFO] Closed server");});
+		tm_log("Removed UNIX socket");
+		tm_log("Closing server");
+		server.close(() => {tm_log("Closed server");});
 		process.removeAllListeners();
 	}
 }
